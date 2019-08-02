f1103c6a90472aa7c7b939ca63cea6ad2d8ee708
巫升清 <sqwu@onlyou.com>
2019-08-02 10:34:17 +0800

    去掉手机模式预览限制

--- public/script/ua.js
+++ public/script/ua.js
@@ -5,5 +5,5 @@ var parser = new UAParser()
 var uaResult = parser.getResult()
 
 if (uaResult.device.type === 'mobile') {
-  window.location.href = './is-mobile.html'
+  //window.location.href = './is-mobile.html'
 }
