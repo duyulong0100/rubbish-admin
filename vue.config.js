557777d3bd92a382339d6a5ac26862d8219b3307
巫升清 <sqwu@onlyou.com>
2019-08-02 14:25:51 +0800

    修改为ng代理

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ function resolve (dir) {
 }
 
 // 基础路径 注意发布之前要先修改这里
-const baseUrl = env === 'development' ? '/' : '/'
+const baseUrl = env === 'development' ? '/' : '/api'
 
 module.exports = {
   baseUrl, // 根据你的实际情况更改这里
